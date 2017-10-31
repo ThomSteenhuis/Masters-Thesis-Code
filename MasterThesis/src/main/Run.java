280e43ec9cea14b88a7531c8d348e3e4d010a593
Thom <l.t.steenhuis@student.rug.nl>
2017-10-31 16:49:10 +0800

    Organized the code in main.Run

--- MasterThesis/src/main/Run.java
+++ MasterThesis/src/main/Run.java
@@ -3,141 +3,44 @@ import models.ExponentialSmoothing;
 import models.Performance;
 
 public class Run {
-	
+	private static final double alphaLBound = 0.01;
+	private static final double alphaRBound = 1;
+	private static final double betaLBound = 0.01;
+	private static final double betaRBound = 1;
+	private static final double gammaLBound = 0.01;
+	private static final double gammaRBound = 1;
 
+	private static final double propTraining = 0.7;
+	private static final double propValidation = 0.2;
+	
 	public static void main(String[] args) 
 	{
 		input.Run.main(args);
 
 		double[][] inverseData = inverse(input.Run.volumes);
 		
-		double[][] SESbounds = new double[1][2];
-		SESbounds[0][0] = 0.01;
-		SESbounds[0][1] = 1;
+		double[][] SESbounds = initBounds("alpha");		
+		double[][] HoltAddbounds = initBounds("beta");
+		double[][] HoltWintersBounds = initBounds("gamma");
 		
-		double[][] HoltAddbounds = new double[2][2];
-		HoltAddbounds[0][0] = 0.01;
-		HoltAddbounds[0][1] = 1;
-		HoltAddbounds[1][0] = 0.01;
-		HoltAddbounds[1][1] = 1;
-		
-		double[][] HoltWintersBounds = new double[3][2];
-		HoltWintersBounds[0][0] = 0.01;
-		HoltWintersBounds[0][1] = 1;
-		HoltWintersBounds[1][0] = 0.01;
-		HoltWintersBounds[1][1] = 1;
-		HoltWintersBounds[2][0] = 0.01;
-		HoltWintersBounds[2][1] = 1;
-		
-		double[] td = new double[(int) (0.7*inverseData[0].length)];
-		double[] vd = new double[(int) (0.2*inverseData[0].length)];
-		
-		for(int idx=0;idx<td.length;++idx)
-			td[idx] = inverseData[0][idx];
-		
-		for(int idx=0;idx<vd.length;++idx)
-			vd[idx] = inverseData[0][td.length+idx];
+		double[] td = initData("training",inverseData[0]);
+		double[] vd = initData("validation",inverseData[0]);
 		
 		optimization.Initialize.initializeOptimization("SES","Grid Search",SESbounds,1,0,td,vd);
 		double[] SESestimate = optimization.Initialize.estVals;
-		
-		System.out.printf("RMSE: %.2f\n",optimization.Initialize.performance);
-		System.out.printf("Optimal par: %.2f\n",optimization.Initialize.optPars[0]);
-		
-		double[] pars = new double[1];
-		pars[0] = 0.05;
-		double[] data = new double[(int) (0.7*inverseData[0].length) + (int) (0.2*inverseData[0].length)];
-		
-		for(int idx=0;idx<data.length;++idx)
-			data[idx] = inverseData[0][idx];
-		
-		double[] ests = ExponentialSmoothing.trainAndValidate(0,pars,1,data);
-		double[] valEsts = new double[(int) (0.2*inverseData[0].length)];
-		
-		for(int idx=0;idx<valEsts.length;++idx)
-			valEsts[idx] = ests[td.length+idx];
-		
-		for(int idx=0;idx<SESestimate.length;++idx)
-			System.out.printf("%.2f %.2f\n",SESestimate[idx],valEsts[idx]);
-		
-		double rmse = models.Performance.validate(0, vd, valEsts);
-		
-		System.out.printf("RMSE: %.2f\n",rmse);
-		System.out.printf("Optimal par: %.2f\n",pars[0]);
+		double[] SESBestPars = optimization.Initialize.optPars;
 		
 		optimization.Initialize.initializeOptimization("Holt additive","Grid Search",HoltAddbounds,1,0,td,vd);
 		double[] DESestimate = optimization.Initialize.estVals;
-		
-		System.out.printf("RMSE: %.2f\n",optimization.Initialize.performance);
-		System.out.printf("Optimal pars: %.2f and %.2f\n",optimization.Initialize.optPars[0],optimization.Initialize.optPars[1]);
-		
-		double[] pars2 = new double[2];
-		pars2[0] = 0.07;
-		pars2[1] = 0.07;
-		
-		double[] ests2 = ExponentialSmoothing.trainAndValidate(1,pars2,1,data);
-		double[] valEsts2 = new double[(int) (0.2*inverseData[0].length)];
-		
-		for(int idx=0;idx<valEsts2.length;++idx)
-			valEsts2[idx] = ests2[td.length+idx];
-		
-		for(int idx=0;idx<SESestimate.length;++idx)
-			System.out.printf("%.2f %.2f\n",DESestimate[idx],valEsts2[idx]);
-		
-		double rmse2 = models.Performance.validate(0, vd, valEsts2);
-		
-		System.out.printf("RMSE: %.2f\n",rmse2);
-		System.out.printf("Optimal pars: %.2f and %.2f\n",pars2[0],pars2[1]);
+		double[] DESBestPars = optimization.Initialize.optPars;
 		
 		optimization.Initialize.initializeOptimization("Holt-Winters additive","Grid Search",HoltWintersBounds,1,0,td,vd);
 		double[] TESestimate1 = optimization.Initialize.estVals;
-		
-		System.out.printf("RMSE: %.2f\n",optimization.Initialize.performance);
-		System.out.printf("Optimal pars: %.2f, %.2f and %.2f\n",optimization.Initialize.optPars[0],optimization.Initialize.optPars[1],optimization.Initialize.optPars[2]);
-		
-		double[] pars3 = new double[3];
-		pars3[0] = 0.01;
-		pars3[1] = 0.14;
-		pars3[2] = 0.51;
-		
-		double[] ests3 = ExponentialSmoothing.trainAndValidate(2,pars3,1,data);
-		double[] valEsts3 = new double[(int) (0.2*inverseData[0].length)];
-		
-		for(int idx=0;idx<valEsts3.length;++idx)
-			valEsts3[idx] = ests3[td.length+idx];
-		
-		for(int idx=0;idx<TESestimate1.length;++idx)
-			System.out.printf("%.2f %.2f\n",TESestimate1[idx],valEsts3[idx]);
-		
-		double rmse3 = models.Performance.validate(0, vd, valEsts3);
-		
-		System.out.printf("RMSE: %.2f\n",rmse3);
-		System.out.printf("Optimal pars: %.2f, %.2f and %.2f\n",pars3[0],pars3[1],pars3[2]);
+		double[] TESBestPars1 = optimization.Initialize.optPars;
 		
 		optimization.Initialize.initializeOptimization("Holt-Winters multiplicative","Grid Search",HoltWintersBounds,1,0,td,vd);
 		double[] TESestimate2 = optimization.Initialize.estVals;
-		
-		System.out.printf("RMSE: %.2f\n",optimization.Initialize.performance);
-		System.out.printf("Optimal pars: %.2f, %.2f and %.2f\n",optimization.Initialize.optPars[0],optimization.Initialize.optPars[1],optimization.Initialize.optPars[2]);
-		
-		double[] pars4 = new double[3];
-		pars4[0] = 0.05;
-		pars4[1] = 0.04;
-		pars4[2] = 0.01;
-		
-		double[] ests4 = ExponentialSmoothing.trainAndValidate(3,pars4,1,data);
-		double[] valEsts4 = new double[(int) (0.2*inverseData[0].length)];
-		
-		for(int idx=0;idx<valEsts4.length;++idx)
-			valEsts4[idx] = ests4[td.length+idx];
-		
-		for(int idx=0;idx<TESestimate2.length;++idx)
-			System.out.printf("%.2f %.2f\n",TESestimate2[idx],valEsts4[idx]);
-		
-		double rmse4 = models.Performance.validate(0, vd, valEsts4);
-		
-		System.out.printf("RMSE: %.2f\n",rmse4);
-		System.out.printf("Optimal pars: %.2f, %.2f and %.2f\n",pars4[0],pars4[1],pars4[2]);
+		double[] TESBestPars2 = optimization.Initialize.optPars;
 		
 		double[][] allData = mergeColVectors(vd,SESestimate);
 		allData = mergeColVectors(allData,DESestimate);
@@ -159,32 +62,114 @@ public class Run {
 		header[3] = "Estimated by Holt-Winters additive";
 		header[4] = "Estimated by Holt-Winters multiplicative";
 		
-		/*		
-		double[] RMSE = new double[4];
-		double[] MAPE = new double[4];
-		double[] MAE = new double[4];
-		double[] ME = new double[4];
-		*/
 		String[] methods = new String[4];
 		
 		methods[0] = "SES";
 		methods[1] = "Holt additive";
 		methods[2] = "Holt-Winters additive";
 		methods[3] = "Holt-Winters multiplicative";
-		/*		
+		
+		double[] RMSE = new double[4];
+		double[] MAPE = new double[4];
+		double[] MAE = new double[4];
+		double[] ME = new double[4];
 		double[][] inverseAllData = inverse(allData);
 		
 		for(int idx=0;idx<4;++idx)
 		{
-			RMSE[idx] = Performance.calculateRMSE(inverseAllData[0],inverseAllData[idx+1]);
-			MAPE[idx] = Performance.calculateMAPE(inverseAllData[0],inverseAllData[idx+1]);
-			MAE[idx] = Performance.calculateMAE(inverseAllData[0],inverseAllData[idx+1]);
-			ME[idx] = Performance.calculateME(inverseAllData[0],inverseAllData[idx+1]);
+			RMSE[idx] = Performance.validate(0,inverseAllData[0],inverseAllData[idx+1]);
+			MAPE[idx] = Performance.validate(1,inverseAllData[0],inverseAllData[idx+1]);
+			MAE[idx] = Performance.validate(2,inverseAllData[0],inverseAllData[idx+1]);
+			ME[idx] = Performance.validate(3,inverseAllData[0],inverseAllData[idx+1]);
 		}
 		
-		Performance.printMeasures(methods,RMSE,MAPE,MAE,ME);*/
+		printBestPars("SES",SESBestPars);
+		Performance.printMeasures(methods,RMSE,MAPE,MAE,ME);
 		
 		graph.Plot.initialize(mode, allData, dates, header, input.Run.labels);
+	}
+	
+	private static void printBestPars(String name,double[] pars)
+	{
+		
+	}
+	
+	private static double[] initData(String set,double[] data)
+	{
+		switch(set)
+		{
+			case "training":
+			{
+				double[] output = new double[(int) (propTraining*data.length)];
+				
+				for(int idx=0;idx<output.length;++idx)
+					output[idx] = data[idx];
+				
+				return output;
+			}
+			case "validation":
+			{
+				double[] output = new double[(int) (propValidation*data.length)];
+				
+				for(int idx=0;idx<output.length;++idx)
+					output[idx] = data[(int) (propTraining*data.length) + idx];
+				
+				return output;
+			}
+			case "testing":
+			{
+				double[] output = new double[data.length - (int) (propTraining*data.length) - (int) (propValidation*data.length)];
+				
+				for(int idx=0;idx<output.length;++idx)
+					output[idx] = data[(int) (propTraining*data.length) + (int) (propValidation*data.length) + idx];
+				
+				return output;
+			}
+			default:
+			{
+				System.out.println("Error (initSet): default case reached");
+				return null;
+			}
+		}
+	}
+	
+	private static double[][] initBounds(String par)
+	{
+		switch(par)
+		{
+			case "alpha":
+			{
+				double[][] output = new double[1][2];
+				output[0][0] = alphaLBound;
+				output[0][1] = alphaRBound;
+				return output;
+			}
+			case "beta":
+			{
+				double[][] output = new double[2][2];
+				output[0][0] = alphaLBound;
+				output[0][1] = alphaRBound;
+				output[1][0] = betaLBound;
+				output[1][1] = betaRBound;
+				return output;
+			}
+			case "gamma":
+			{
+				double[][] output = new double[3][2];
+				output[0][0] = alphaLBound;
+				output[0][1] = alphaRBound;
+				output[1][0] = betaLBound;
+				output[1][1] = betaRBound;
+				output[2][0] = gammaLBound;
+				output[2][1] = gammaRBound;
+				return output;
+			}
+			default:
+			{
+				System.out.println("Error (initBounds): default case reached");
+				return null;
+			}
+		}
 	}
 
 	private static double[][] inverse(double[][] input)
