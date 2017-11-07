5bfef68c6fbfd4fc0a16a68243470dd54c96629b
Thom <l.t.steenhuis@student.rug.nl>
2017-11-07 15:02:40 +0800

    Added SVR class

---
+++ MasterThesis/src/models/SVR.java
@@ -0,0 +1,30 @@
+package models;
+
+import input.Data;
+
+public class SVR extends Model {
+
+	public SVR(Data dataset, int periods) {
+		super(dataset, periods);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void train() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void validate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
