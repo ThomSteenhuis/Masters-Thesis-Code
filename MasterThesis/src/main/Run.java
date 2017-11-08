91cb4d1ecb51f043026e915a1dee1ffe73cac406
Thom Steenhuis <l.t.steenhuis@student.rug.nl>
2017-11-08 19:45:26 +0800

    Fixed the convergence problem for the ellipsoid method

--- MasterThesis/src/main/Run.java
+++ MasterThesis/src/main/Run.java
@@ -11,23 +11,30 @@ public class Run {
 
 	private static final double propTraining = 0.6;
 	private static final double propValidation = 0.2;
-	
-	public static void main(String[] args) 
+
+	public static void main(String[] args)
 	{
 		Data data = new Data("src/data/prepared_data.txt");
 		data.setDataIndices(propTraining, propValidation);
-		
+
 		SVR testModel = new SVR(data,1);
-		double[] pars = {1,1,1,3};
+		double[] pars = {1,1,1,6};
 		testModel.setParameters(pars);
 		testModel.setCategory("2200EVO");
 		testModel.train();
-		
+		testModel.printLambda();
+
+		double sum = 0;
+		for(int idx=0;idx<testModel.getN();++idx)
+			sum += testModel.getLambda()[idx];
+
+		System.out.println(sum);
+
 		/*ExponentialSmoothing testModel = new ExponentialSmoothing("four",true,true,1,data);
 		double[] cons = {12};
 		testModel.setConstants(cons);
 		PerformanceMeasures pm = new PerformanceMeasures(testModel);
-		double[][] SESbounds = initBounds(4);	
+		double[][] SESbounds = initBounds(4);
 		GridSearch gs = new GridSearch(pm,SESbounds,40);
 		gs.optimize("2200EVO");
 		gs.printBest();
@@ -36,75 +43,75 @@ public class Run {
 		pm.calculateMeasures();
 		pm.printMeasures();
 		testModel.plotForecast("validation");
-		
-		
+
+
 		double[] pars = {0.01,0.21,0.01,0.01};
 		testModel.setParameters(pars);
 		testModel.setCategory("2200EVO");
 		testModel.train();
 		pm.calculateMeasures();
 		pm.printMeasures();*/
-		
+
 		/*input.Run.main(args);
 
 		double[][] inverseData = inverse(input.Run.volumes);
-		
-			
+
+
 		double[][] HoltAddbounds = initBounds("beta");
 		double[][] HoltWintersBounds = initBounds("gamma");
-		
+
 		double[] td = initData("training",inverseData[0]);
 		double[] vd = initData("validation",inverseData[0]);
-		
+
 		optimization.Initialize.initializeOptimization("SES","Grid Search",SESbounds,1,0,td,vd);
 		double[] SESestimate = optimization.Initialize.estVals;
 		double[] SESBestPars = optimization.Initialize.optPars;
-		
+
 		optimization.Initialize.initializeOptimization("Holt additive","Grid Search",HoltAddbounds,1,0,td,vd);
 		double[] DESestimate = optimization.Initialize.estVals;
 		double[] DESBestPars = optimization.Initialize.optPars;
-		
+
 		optimization.Initialize.initializeOptimization("Holt-Winters additive","Grid Search",HoltWintersBounds,1,0,td,vd);
 		double[] TESestimate1 = optimization.Initialize.estVals;
 		double[] TESBestPars1 = optimization.Initialize.optPars;
-		
+
 		optimization.Initialize.initializeOptimization("Holt-Winters multiplicative","Grid Search",HoltWintersBounds,1,0,td,vd);
 		double[] TESestimate2 = optimization.Initialize.estVals;
 		double[] TESBestPars2 = optimization.Initialize.optPars;
-		
+
 		double[][] allData = mergeColVectors(vd,SESestimate);
 		allData = mergeColVectors(allData,DESestimate);
 		allData = mergeColVectors(allData,TESestimate1);
 		allData = mergeColVectors(allData,TESestimate2);
-		
+
 		String[] mode = new String[1];
 		mode[0] = "pivot";
-		
+
 		String[] dates = new String[vd.length];
-		
+
 		for(int idx=0;idx<vd.length;++idx)
 			dates[idx] = input.Run.dates[td.length+idx];
-		
+
 		String[] header = new String[5];
 		header[0] = "Real values";
 		header[1] = "Estimated by SES";
 		header[2] = "Estimated by Holt additive";
 		header[3] = "Estimated by Holt-Winters additive";
 		header[4] = "Estimated by Holt-Winters multiplicative";
-		
+
 		String[] methods = new String[4];
-		
+
 		methods[0] = "SES";
 		methods[1] = "Holt additive";
 		methods[2] = "Holt-Winters additive";
 		methods[3] = "Holt-Winters multiplicative";
-		
+
 		double[] RMSE = new double[4];
 		double[] MAPE = new double[4];
 		double[] MAE = new double[4];
 		double[] ME = new double[4];
 		double[][] inverseAllData = inverse(allData);
-		
+
 		for(int idx=0;idx<4;++idx)
 		{
 			RMSE[idx] = Performance.validate(0,inverseAllData[0],inverseAllData[idx+1]);
@@ -112,13 +119,13 @@ public class Run {
 			MAE[idx] = Performance.validate(2,inverseAllData[0],inverseAllData[idx+1]);
 			ME[idx] = Performance.validate(3,inverseAllData[0],inverseAllData[idx+1]);
 		}
-		
+
 		printBestPars("SES",SESBestPars);
 		Performance.printMeasures(methods,RMSE,MAPE,MAE,ME);
-		
+
 		graph.Plot.initialize(mode, allData, dates, header, input.Run.labels);*/
 	}
-	
+
 	private static double[] initData(String set,double[] data)
 	{
 		switch(set)
@@ -126,28 +133,28 @@ public class Run {
 			case "training":
 			{
 				double[] output = new double[(int) (propTraining*data.length)];
-				
+
 				for(int idx=0;idx<output.length;++idx)
 					output[idx] = data[idx];
-				
+
 				return output;
 			}
 			case "validation":
 			{
 				double[] output = new double[(int) (propValidation*data.length)];
-				
+
 				for(int idx=0;idx<output.length;++idx)
 					output[idx] = data[(int) (propTraining*data.length) + idx];
-				
+
 				return output;
 			}
 			case "testing":
 			{
 				double[] output = new double[data.length - (int) (propTraining*data.length) - (int) (propValidation*data.length)];
-				
+
 				for(int idx=0;idx<output.length;++idx)
 					output[idx] = data[(int) (propTraining*data.length) + (int) (propValidation*data.length) + idx];
-				
+
 				return output;
 			}
 			default:
@@ -157,17 +164,17 @@ public class Run {
 			}
 		}
 	}
-	
+
 	private static double[][] initBounds(int no)
 	{
 		double[][] output = new double[no][2];
-		
+
 		for(int idx=0;idx<no;++idx)
 		{
 			output[idx][0] = lowerBound;
 			output[idx][1] = upperBound;
 		}
-		
+
 		return output;
 	}
 
@@ -178,12 +185,12 @@ public class Run {
 			inverseError("input is of length 0");
 			return null;
 		}
-		
+
 		int noLines = input.length;
 		int noColumns = input[0].length;
-		
+
 		double[][] output = new double[noColumns][noLines];
-		
+
 		for(int idx1=0;idx1<noLines;++idx1)
 		{
 			if(input[idx1].length != noColumns)
@@ -191,38 +198,38 @@ public class Run {
 				inverseError("input does not have the same no of columns everywhere");
 				return null;
 			}
-			
+
 			for(int idx2=0;idx2<noColumns;++idx2)
 				output[idx2][idx1] = input[idx1][idx2];
 		}
-		
+
 		return output;
 	}
-	
+
 	private static void inverseError(String txt)
 	{
 		System.out.println("Error (inverse): "+ txt);
 	}
-	
+
 	private static double[][] mergeColVectors(double[] vec1, double[] vec2)
 	{
 		if(vec1.length != vec2.length)
 		{
 			System.out.println("Error (mergeColVectors): vectors not of the same length");
 		}
-		
+
 		int noLines = vec1.length;
 		double[][] output = new double[noLines][2];
-		
+
 		for(int idx=0;idx<noLines;++idx)
 		{
 			output[idx][0] = vec1[idx];
 			output[idx][1] = vec2[idx];
 		}
-		
+
 		return output;
 	}
-	
+
 	private static double[][] mergeColVectors(double[][] mat1, double[] vec2)
 	{
 		if( (mat1.length != vec2.length) && (mat1.length != 0) )
@@ -230,37 +237,37 @@ public class Run {
 			System.out.println("Error (mergeColVectors): vectors not of the same length or of length 0");
 			return null;
 		}
-		
+
 		int noLines = mat1.length;
 		int noCols = mat1[0].length;
 		double[][] output = new double[noLines][noCols+1];
-		
+
 		for(int idx1=0;idx1<noLines;++idx1)
 		{
 			for(int idx2=0;idx2<noCols;++idx2)
 				output[idx1][idx2] = mat1[idx1][idx2];
-			
+
 			output[idx1][noCols] = vec2[idx1];
 		}
-		
+
 		return output;
 	}
-	
+
 	private static void print(double[] estimates, double[] real)
 	{
 		if(estimates.length != real.length)
 		{
 			System.out.println("Error (print): vectors not of the same length");
 		}
-		
+
 		System.out.print("Estimates:\tReal:\n");
-		
+
 		for(int idx=0;idx<real.length;++idx)
 		{
 			System.out.printf("%.2f\t%.2f\n",estimates[idx],real[idx]);
 		}
 	}
-	
+
 	private static void print(double[] values)
 	{
 		for(int idx=0;idx<values.length;++idx)
--- MasterThesis/src/models/SVR.java
+++ MasterThesis/src/models/SVR.java
@@ -1,19 +1,21 @@
 package models;
 
+import java.util.Random;
+
 import input.Data;
 import math.Matrix;
 
 public class SVR extends Model {
-	
+
 	private double[][] x;
 	private double[] y;
 	private int N;
 	private double[] lambda;
-	
-	private final double constraintError = 0.001;
-	private final double stoppingError = 0.000001;
 
-	public SVR(Data dataset, int periods) 
+	private final double stoppingError = 0.001;
+	private final int maxIters = 100000;
+
+	public SVR(Data dataset, int periods)
 	{
 		super(dataset, periods);
 		name = "SVR";
@@ -21,7 +23,7 @@ public class SVR extends Model {
 		noConstants = 0;
 	}
 
-	public void train() 
+	public void train()
 	{
 		initializeXY();
 		ellipsoidMethod();
@@ -30,25 +32,33 @@ public class SVR extends Model {
 	public void validate() {}
 
 	public void test() {}
-	
+
 	public double[][] getX()
 	{
 		return x;
 	}
-	
+
 	public double[] getY()
 	{
 		return y;
 	}
-	
+
 	public int getN()
 	{
 		return N;
 	}
-	
+
 	public double[] getLambda()
 	{
 		return lambda;
+	}
+
+	public void printLambda()
+	{
+		for(int idx=0;idx<N;++idx)
+			System.out.printf("%f\t",lambda[idx]);
+
+		System.out.println();
 	}
 
 	private void initializeXY()
@@ -57,18 +67,18 @@ public class SVR extends Model {
 		x = new double[N][(int) (parameters[3])];
 		y = new double[N];
 		int index = data.getIndexFromCat(category);
-		
+
 		for(int idx1=0;idx1<N;++idx1)
 		{
 			y[idx1] = data.getVolumes()[idx1 + (int) (parameters[3]) + noPersAhead][index];
-			
+
 			for(int idx2=0;idx2<x[idx1].length;++idx2)
 			{
 				x[idx1][idx2] =  data.getVolumes()[idx1 + idx2][index];
 			}
 		}
 	}
-	
+
 	private void ellipsoidMethod()
 	{
 		double[] center = initializeCenter();
@@ -77,147 +87,93 @@ public class SVR extends Model {
 		double[] bestCenter = new double[N];
 		boolean firstValue = true;
 		boolean stop = false;
-		int count = 0;
-		
+		int iter = 0;
+
 		while(!stop)
 		{
-			count ++;
+			iter ++;
 			double[] subDifferential;
 			double alpha = 0;
 			double normCoeff = 0;
 			int infeasibleConstraint = checkFeasible(center);
-			
+
 			if(infeasibleConstraint == 0)
 			{
 				subDifferential = new double[N];
-				
+
 				for(int idx1=0;idx1<N;++idx1)
 				{
 					double sign = center[idx1] / Math.abs(center[idx1]);
 					subDifferential[idx1] = parameters[1] * sign - y[idx1];
-					
+
 					for(int idx2=0;idx2<N;++idx2)
 						subDifferential[idx1] += (center[idx2] * kernel(x[idx1],x[idx2]) );
 				}
-				
+
 				normCoeff = Matrix.innerProduct(subDifferential,Matrix.innerProduct(ellipsMatrix,subDifferential) );
 				normCoeff = 1 / Math.sqrt(normCoeff);
-				
-				for(int idx=0;idx<N;++idx)
-					subDifferential[idx] = subDifferential[idx] * normCoeff;
-				
-				double currentValue = 0;
-				
-				for(int idx1=0;idx1<N;++idx1)
-				{
-					currentValue += parameters[1] * Math.abs(center[idx1]) - y[idx1] * center[idx1] + 0.5 * Math.pow(center[idx1] , 2);
-					
-					for(int idx2=0;idx2<idx1;++idx2)
-						currentValue += center[idx1] * center[idx2] * kernel(x[idx1],x[idx2]);
-				}
-				
-				System.out.printf("Current value = %.2f\n",currentValue);
-				/*for(int idx=0;idx<N;++idx)
-				{
-					System.out.printf("%.2f\t", center[idx]);
-				}*/
-				
+				subDifferential = Matrix.scalarMultiplication(normCoeff,subDifferential);
+
+				double currentValue = objective(center);
+
 				if( firstValue || (currentValue < bestValue) )
 				{
 					bestValue = currentValue;
-					System.out.printf("Best value = %.2f\n",bestValue);
-					
 					bestCenter = new double[N];
-					
+
 					for(int idx=0;idx<N;++idx)
-					{
 						bestCenter[idx] = center[idx];
-						System.out.printf("%.2f\t", bestCenter[idx]);
-					}
-					System.out.println();
-					
+
 					firstValue = false;
 				}
-				
+
 				alpha = (currentValue - bestValue) * normCoeff;
 			}
 			else
 			{
-				System.out.printf("Infeasible constraint = %d at iteration %d\n",infeasibleConstraint,count);
-				
-				double sum=0;
-				for(int idx=0;idx<N;++idx)
-				{
-					sum+=center[idx];
-					System.out.printf("%f\t", center[idx]);
-				}
-				System.out.println();
-				System.out.println(sum);
-				System.out.println(parameters[0]/(N+1));
-				
 				subDifferential = subDiffConstraint(infeasibleConstraint);
-				
-				for(int idx=0;idx<N;++idx)
-				{
-					System.out.printf("%f\t", subDifferential[idx]);
-				}
-				System.out.println();
-				
 				normCoeff = Matrix.innerProduct(subDifferential,Matrix.innerProduct(ellipsMatrix,subDifferential) );
 				normCoeff = 1 / Math.sqrt(normCoeff);
-				
-				for(int idx=0;idx<N;++idx)
-					subDifferential[idx] = subDifferential[idx] * normCoeff;
-				
+				subDifferential = Matrix.scalarMultiplication(normCoeff,subDifferential);
 				alpha = currentConstraint(infeasibleConstraint,center) * normCoeff;
 			}
-			
-			double[] pars1 = {1,1,1};
-			double[] pars2 = {2,2,2};
-			/*System.out.printf("%f\n", kernel(pars1,pars2));
-			
-			System.out.println(normCoeff);	
-			
-			for(int idx1=0;idx1<N;++idx1)
-			{
-				System.out.printf("%f\t",subDifferential[idx1]);
-			}
-			System.out.println();
-			
-			System.out.println(alpha);
-			
-			for(int idx1=0;idx1<N;++idx1)
-			{
-				for(int idx2=0;idx2<N;++idx2)
-				{
-					System.out.printf("%f\t",ellipsMatrix[idx1][idx2]);
-				}
-				System.out.println();
-			}*/
-			
+
 			double scalar1 = (1 + N * alpha) / (N + 1);
 			double scalar2 = Math.pow(N,2) / (Math.pow(N,2) - 1) * (1 - Math.pow(alpha,2) );
 			double scalar3 = ( (2*(1+N*alpha) ) / ( (N+1) * (1+alpha) ) );
-			
+
 			double[] ellMat_subDiff = Matrix.innerProduct(ellipsMatrix,subDifferential);
 			center = Matrix.difference(center,Matrix.scalarMultiplication(scalar1,ellMat_subDiff) );
 			ellipsMatrix = Matrix.scalarMultiplication(scalar2,(Matrix.difference(ellipsMatrix,Matrix.scalarMultiplication(scalar3,Matrix.outerProduct(ellMat_subDiff,ellMat_subDiff) ) ) ) );
-		
-			System.out.println(1/normCoeff);
-			
-			if( ( (1/normCoeff) < stoppingError) && !firstValue || count == 5000 )
+
+			if( ( (1/normCoeff) < stoppingError) && !firstValue || iter == maxIters )
 				stop = true;
 		}
-		
-		System.out.printf("Stopped at iteration %d\n", count);
-		
+
+		System.out.printf("Stopped at iteration %d. Best value = %f\n", iter,bestValue);
+
 		lambda = bestCenter;
 	}
-	
+
+	public double objective(double[] input)
+	{
+		double output = 0;
+
+		for(int idx1=0;idx1<N;++idx1)
+		{
+			output += parameters[1] * Math.abs(input[idx1]) - y[idx1] * input[idx1] + 0.5 * Math.pow(input[idx1] , 2);
+
+			for(int idx2=0;idx2<idx1;++idx2)
+				output += input[idx1] * input[idx2] * kernel(x[idx1],x[idx2]);
+		}
+
+		return output;
+	}
+
 	private double[] subDiffConstraint(int no)
 	{
 		double[] output = new double[N];
-		
+
 		if(no == (N+1) )
 		{
 			for(int idx=0;idx<N;++idx)
@@ -232,28 +188,28 @@ public class SVR extends Model {
 			output[no-1] = 1;
 		else
 			output[-no-1] = -1;
-		
+
 		return output;
 	}
-	
+
 	private double currentConstraint(int no,double[] center)
-	{		
+	{
 		if(no == (N+1) )
 		{
-			double output = -constraintError;
-			
+			double output = -stoppingError;
+
 			for(int idx=0;idx<N;++idx)
 				output += center[idx];
-			
+
 			return output;
 		}
 		else if(no == (-N-1))
 		{
-			double output = -constraintError;
-			
+			double output = -stoppingError;
+
 			for(int idx=0;idx<N;++idx)
 				output -= center[idx];
-			
+
 			return output;
 		}
 		else if(no>0)
@@ -261,69 +217,70 @@ public class SVR extends Model {
 		else
 			return (-center[-no-1] - parameters[0]);
 	}
-	
+
 	private double kernel(double[] first,double[] second)
 	{
 		double[] diff = Matrix.difference(first,second);
-		
+
 		double k = Math.exp( -Matrix.innerProduct(diff, diff) / (2 * parameters[2] ) );
-		
+
 		if(Double.isInfinite(k))
 			return Double.MAX_VALUE;
 		else
 			return k;
 	}
-	
+
 	private int checkFeasible(double[] c)
 	{
 		double sum = 0;
-		
+
 		for(int idx=0;idx<N;++idx)
 		{
-			if(c[idx] > parameters[0]) 
+			if(c[idx] > parameters[0])
 				return (idx+1);
-			
+
 			if(c[idx] < (-parameters[0]) )
 				return -(idx+1);
-			
+
 			sum += c[idx];
 		}
-		
-		if(sum > constraintError)
+
+		if(sum > stoppingError)
 			return N+1;
-		
-		if(sum < (-constraintError) )
+
+		if(sum < (-stoppingError) )
 			return -N-1;
-		
+
 		return 0;
 	}
-	
+
 	private double[] initializeCenter()
 	{
 		double[] c = new double[N];
 		double sum = 0;
 		double boundary = parameters[0]/(N-1);
-		
+		Random r = new Random();
+
 		for(int idx=0;idx<(N-1);++idx)
 		{
-			c[idx] = boundary * (2 * Math.random() - 1);
+			c[idx] = boundary * (2 * r.nextDouble() - 1);
 			sum += c[idx];
 		}
-		
+
 		c[N-1] = -sum;
 		return c;
 	}
-	
+
 	private double[][] initializeEllipsMatrix(double[] c)
 	{
 		double[][] P = new double[N][N];
-		double maxRadius = Math.pow(2*parameters[0],2) + (N-1) * Math.pow(2*constraintError,2);
-		
+		double maxRadius = Math.pow(2*parameters[0],2) + (N-1) * Math.pow(2*stoppingError,2);
+
 		for(int idx=0;idx<N;++idx)
 		{
-			P[idx][idx] = 1/maxRadius;
+			P[idx][idx] = 1000;
 		}
-		
+
 		return P;
 	}
 }
