ccf8f4491b04b51b172635aece8486e699c1e0c6
Thom <l.t.steenhuis@student.rug.nl>
2017-11-08 15:02:33 +0800

    implemented ellipsoid method for solving SVR

---
+++ MasterThesis/src/math/Matrix.java
@@ -0,0 +1,155 @@
+package math;
+
+public class Matrix {
+
+	public static double innerProduct(double[] array1, double[] array2)
+	{
+		if(array1.length != array2.length)
+		{
+			System.out.println("Error (innerProduct): arrays have unequal length");
+			return 0;
+		}
+		
+		double output = 0;
+		
+		for(int idx=0;idx<array1.length;++idx)
+		{
+			output += (array1[idx] * array2[idx]);
+		}
+		
+		return output;
+	}
+	
+	public static double[] innerProduct(double[] array, double[][] matrix)
+	{
+		if( (array.length != matrix.length) || (array.length == 0) )
+		{
+			System.out.println("Error (innerProduct): array and matrix have unequal length or 0 length");
+			return null;
+		}
+		
+		double[] output = new double[matrix[0].length];
+		
+		for(int idx1=0;idx1<matrix[0].length;++idx1)
+		{
+			for(int idx2=0;idx2<array.length;++idx2)
+			{
+				output[idx1] += (array[idx2] * matrix[idx2][idx1]);
+			}
+		}
+		
+		return output;
+	}
+	
+	public static double[] innerProduct(double[][] matrix, double[] array)
+	{
+		if(matrix.length == 0)
+		{
+			System.out.println("Error (innerProduct): matrix has 0 length");
+			return null;
+		}
+		
+		if( (array.length != matrix[0].length) || (array.length == 0) )
+		{
+			System.out.println("Error (innerProduct): array and matrix have unequal length or 0 length");
+			return null;
+		}
+		
+		double[] output = new double[matrix.length];
+		
+		for(int idx1=0;idx1<matrix.length;++idx1)
+		{
+			for(int idx2=0;idx2<array.length;++idx2)
+			{
+				output[idx1] += (array[idx2] * matrix[idx1][idx2]);
+			}
+		}
+		
+		return output;
+	}
+	
+	public static double[][] outerProduct(double[] array1, double[] array2)
+	{
+		double[][] output = new double[array1.length][array2.length];
+		
+		for(int idx1=0;idx1<array1.length;++idx1)
+		{
+			for(int idx2=0;idx2<array2.length;++idx2)
+			{
+				output[idx1][idx2] = array1[idx1] * array2[idx2];
+			}
+		}
+		
+		return output;
+	}
+	
+	public static double[] difference(double[] array1, double[] array2)
+	{
+		if(array1.length != array2.length)
+		{
+			System.out.println("Error (difference): arrays have unequal length");
+			return null;
+		}
+		
+		double[] output = new double[array1.length];
+		
+		for(int idx=0;idx<array1.length;++idx)
+		{
+			output[idx] = array1[idx] - array2[idx];
+		}
+		
+		return output;
+	}
+	
+	public static double[][] difference(double[][] matrix1, double[][] matrix2)
+	{
+		if( (matrix1.length != matrix2.length) || (matrix1.length == 0) )
+		{
+			System.out.println("Error (difference): matrixs have unequal length or 0 length");
+			return null;
+		}
+		
+		double[][] output = new double[matrix1.length][matrix1[0].length];
+		
+		for(int idx1=0;idx1<matrix1.length;++idx1)
+		{
+			for(int idx2=0;idx2<matrix1[0].length;++idx2)
+			{
+				output[idx1][idx2] = matrix1[idx1][idx2] - matrix2[idx1][idx2];
+			}
+		}
+		
+		return output;
+	}
+	
+	public static double[] scalarMultiplication(double scalar, double[] array)
+	{
+		double[] output = new double[array.length];
+		
+		for(int idx=0;idx<array.length;++idx)
+			output[idx] = scalar * array[idx];
+		
+		return output;
+	}
+	
+	public static double[][] scalarMultiplication(double scalar, double[][] matrix)
+	{
+		if(matrix.length == 0)
+		{
+			System.out.println("Error (scalarMultiplication): matrix has 0 length");
+			return null;
+		}
+		
+		double[][] output = new double[matrix.length][matrix[0].length];
+		
+		for(int idx1=0;idx1<matrix.length;++idx1)
+		{
+			for(int idx2=0;idx2<matrix[0].length;++idx2)
+			{
+				output[idx1][idx2] = scalar * matrix[idx1][idx2];
+			}
+		}			
+		
+		return output;
+	}
+}
--- MasterThesis/src/models/SVR.java
+++ MasterThesis/src/models/SVR.java
@@ -1,6 +1,7 @@
 package models;
 
 import input.Data;
+import math.Matrix;
 
 public class SVR extends Model {
 	
@@ -52,40 +53,145 @@ public class SVR extends Model {
 	{
 		double[] center = initializeCenter();
 		double[][] ellipsMatrix = initializeEllipsMatrix(center);
-		double bestValue;
-		boolean firstValue = false;
+		double bestValue = 0;
+		boolean firstValue = true;
 		boolean stop = false;
 		
 		while(!stop)
 		{
-			if(checkFeasible(center))
+			double[] subDifferential = new double[N];
+			double[] ellMat_subDiff = new double[N];
+			double alpha = 0;
+			double normCoeff = 0;
+			int infeasibleConstraint = checkFeasible(center);
+			
+			if(infeasibleConstraint == 0)
 			{
+				for(int idx1=0;idx1<N;++idx1)
+				{
+					double sign = center[idx1] / Math.abs(center[idx1]);
+					subDifferential[idx1] = parameters[1] * sign - y[idx1];
+					
+					for(int idx2=0;idx2<N;++idx2)
+						subDifferential[idx1] += center[idx2] + kernel(x[idx1],x[idx2]);
+				}
 				
+				ellMat_subDiff = Matrix.innerProduct(ellipsMatrix,subDifferential);
+				normCoeff = Matrix.innerProduct(subDifferential,ellMat_subDiff);
+				normCoeff = 1 / Math.sqrt(normCoeff);
+				
+				for(int idx=0;idx<N;++idx)
+					subDifferential[idx] = subDifferential[idx] / normCoeff;
+				
+				double currentValue = 0;
+				
+				for(int idx1=0;idx1<N;++idx1)
+				{
+					currentValue += parameters[1] * Math.abs(center[idx1]) + y[idx1] * center[idx1] + 0.5 * Math.pow(center[idx1] , 2);
+					
+					for(int idx2=0;idx2<idx1;++idx2)
+						currentValue += center[idx1] * center[idx2] * kernel(x[idx1],x[idx2]);
+				}
+				
+				if( firstValue || (currentValue < bestValue) )
+				{
+					bestValue = currentValue;
+					firstValue = false;
+				}
+				
+				alpha = (currentValue - bestValue) * normCoeff;
 			}
 			else
 			{
+				subDifferential = subDiffConstraint(infeasibleConstraint);
+				ellMat_subDiff = Matrix.innerProduct(ellipsMatrix,subDifferential);
+				normCoeff = Matrix.innerProduct(subDifferential,ellMat_subDiff);
+				normCoeff = 1 / Math.sqrt(normCoeff);
 				
+				for(int idx=0;idx<N;++idx)
+					subDifferential[idx] = subDifferential[idx] / normCoeff;
+				
+				alpha = currentConstraint(infeasibleConstraint,center) * normCoeff;
 			}
+			
+			double scalar1 = (1 + N * alpha) / (N + 1);
+			double scalar2 = Math.pow(N,2) / (Math.pow(N,2) - 1) * (1 - Math.pow(alpha,2) );
+			double scalar3 = ( (2*(1+N*alpha) ) / ( (N+1) * (1+alpha) ) );
+			
+			center = Matrix.difference(center,Matrix.scalarMultiplication(scalar1,ellMat_subDiff) );
+			ellipsMatrix = Matrix.scalarMultiplication(scalar2,(Matrix.difference(ellipsMatrix,Matrix.scalarMultiplication(scalar3,Matrix.outerProduct(ellMat_subDiff,ellMat_subDiff) ) ) ) );
+		
+			if( ( (1/normCoeff) < stoppingError) && !firstValue)
+				stop = true;
 		}
 		
 	}
 	
-	private boolean checkFeasible(double[] c)
+	private double[] subDiffConstraint(int no)
+	{
+		double[] output = new double[N];
+		
+		if(no == (N+1) )
+		{
+			for(int idx=0;idx<N;++idx)
+				output[idx] = 1;
+		}
+		else if(no>0)
+			output[no-1] = 1;
+		else
+			output[-no-1] = -1;
+		
+		return output;
+	}
+	
+	private double currentConstraint(int no,double[] center)
+	{		
+		if(no == (N+1) )
+		{
+			double output = 0;
+			
+			for(int idx=0;idx<N;++idx)
+				output += center[idx];
+			
+			return output;
+		}
+		else if(no>0)
+			return (center[no-1] - parameters[0]);
+		else
+			return (-center[-no-1] - parameters[0]);
+	}
+	
+	private double kernel(double[] first,double[] second)
+	{
+		double[] diff = Matrix.difference(first,second);
+		
+		double k = Math.exp( -Matrix.innerProduct(diff, diff) / (2 * Math.pow(parameters[2], 2) ) );
+		
+		if(Double.isInfinite(k))
+			return Double.MAX_VALUE;
+		else
+			return k;
+	}
+	
+	private int checkFeasible(double[] c)
 	{
 		double sum = 0;
 		
 		for(int idx=0;idx<N;++idx)
 		{
-			if( (c[idx] > parameters[0]) || (c[idx] < (-parameters[0]) ) )
-				return false;
+			if(c[idx] > parameters[0]) 
+				return (idx+1);
+			
+			if(c[idx] < (-parameters[0]) )
+				return -(idx+1);
 			
 			sum += c[idx];
 		}
 		
 		if( (sum > constraintError) || (sum < (-constraintError) ) )
-			return false;
+			return N+1;
 		
-		return true;
+		return 0;
 	}
 	
 	private double[] initializeCenter()
